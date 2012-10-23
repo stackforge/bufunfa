375c2a7df593190c10cd256afe7cdf4241b490b1
Endre Karlson <endre.karlson@gmail.com>
2012-10-23 09:58:36 +0000

    Setup file, requires and bin

---
+++ setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# Copyright 2012 Bouvet ASA
+#
+# Author: Endre Karlson <endre.karlson@bouvet.no>
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+from setuptools import setup, find_packages
+from billistix.openstack.common import setup as common_setup
+
+install_requires = common_setup.parse_requirements(['tools/pip-requires'])
+tests_require = common_setup.parse_requirements(['tools/test-requires'])
+setup_require = common_setup.parse_requirements(['tools/setup-requires'])
+dependency_links = common_setup.parse_dependency_links([
+    'tools/pip-requires',
+    'tools/test-requires',
+    'tools/setup-requires'
+])
+
+setup(
+    name='billistix',
+    version='0.0',
+    description='Billing as a Service',
+    author='Endre Karlson',
+    author_email='endre.karlson@bouvet.no',
+    url='https://launchpad.net/billistix',
+    packages=find_packages(exclude=['bin']),
+    include_package_data=True,
+    test_suite='nose.collector',
+    setup_requires=setup_require,
+    install_requires=install_requires,
+    tests_require=tests_require,
+    extras_require={'test': tests_require},
+    dependency_links=dependency_links,
+    scripts=[
+        'bin/billistix-server',
+    ],
+    cmdclass=common_setup.get_cmdclass(),
+)
