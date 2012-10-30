084f4bb9c279c1c1d9aa06006da39c8c2a5fc504
Endre Karlson <endre.karlson@gmail.com>
2012-10-30 11:04:52 +0000

    Base central service

---
+++ billistix/central/api.py
@@ -0,0 +1,56 @@
+from billistix.openstack.common import cfg
+from billistix.openstack.common import log as logging
+from billistix.openstack.common.rpc.proxy import RpcProxy
+
+DEFAULT_VERSION = "1.0"
+
+LOG = logging.getLogger(__name__)
+RPC = RpcProxy(cfg.CONF.central_topic, DEFAULT_VERSION)
+
+
+def add_rate(context, values):
+    msg = {
+        "method": "add_rate",
+        "args": {
+            "values": values
+        }
+    }
+    return RPC.call(context, msg)
+
+
+def get_rates(context):
+    msg = {
+        "method": "get_rates",
+    }
+    return RPC.call(context, msg)
+
+
+def update_rate(context, rate_id, values):
+    msg = {
+        "method": "update_rate",
+        "args": {
+            "rate_id": rate_id,
+            "values": values
+        }
+    }
+    return RPC.call(context, msg)
+
+
+def delete_rate(context, rate_id):
+    msg = {
+        "method": "delete_rate",
+        "args": {
+            "rate_id": rate_id
+        }
+    }
+    return RPC.call(context, msg)
+
+
+def store_item(context, values):
+    msg = {
+        "method": "store_item",
+        "args": {
+            "item": values
+        }
+    }
+    return RPC.call(context, msg)
---
+++ billistix/central/service.py
@@ -0,0 +1,30 @@
+from billistix.openstack.common import cfg
+from billistix.openstack.common import log
+from billistix.openstack.common.rpc import service as rpc_service
+from billistix import storage
+
+
+LOG = log.getLogger(__name__)
+
+
+class Service(rpc_service.Service):
+    def __init__(self, *args, **kw):
+        kw.update(
+            host=cfg.CONF.host,
+            topic=cfg.CONF.central_topic)
+
+        super(Service, self).__init__(*args, **kw)
+
+        self.storage_conn = storage.get_connection(cfg.CONF)
+
+    def add_rate(self, context, values):
+        return self.storage_conn.add_rate(context, values)
+
+    def get_rates(self, context):
+        return self.storage_conn.get_rate(context, rate_id)
+
+    def update_rate(self, context, rate_id, values):
+        return self.storage_conn.update_rate(context, rate_id, values)
+
+    def delete_rate(self, context, rate_id):
+        return self.storage_conn.delete_rate(context, rate_id)
