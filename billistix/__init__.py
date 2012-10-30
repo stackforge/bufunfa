03198f2ccce61f395da98b88098d4234231b34f2
Endre Karlson <endre.karlson@gmail.com>
2012-10-30 11:05:10 +0000

    Add options

---
+++ billistix/__init__.py
@@ -0,0 +1,19 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+import os
+import socket
+from billistix.openstack.common import cfg
+
+
+cfg.CONF.register_opts([
+    cfg.StrOpt('pybasedir',
+               default=os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                                    '../')),
+               help='Directory where the nova python module is installed'),
+    cfg.StrOpt('host', default=socket.gethostname(),
+               help='Name of this node'),
+    cfg.StrOpt('control-exchange', default='billistix',
+               help='AMQP exchange to connect to if using RabbitMQ or Qpid'),
+    cfg.StrOpt('central-topic', default='central', help='Central Topic'),
+    cfg.StrOpt('worker-topic', default='worker', help='Worker Topic'),
+    cfg.StrOpt('state-path', default='$pybasedir', help='State Path')
+])
